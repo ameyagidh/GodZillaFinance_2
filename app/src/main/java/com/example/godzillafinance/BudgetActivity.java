9ac375fb70a28c44d48838aa45c1be419e8c6f1e
Ameya Santosh Gidh <ameyagb2@gmail.com>
2022-09-05 18:35:42 -0400

    Budget Activity Adding to database.

--- app/src/main/java/com/example/godzillafinance/BudgetActivity.java
+++ app/src/main/java/com/example/godzillafinance/BudgetActivity.java
@@ -1,26 +1,54 @@
 package com.example.godzillafinance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.joda.time.DateTime;
+import org.joda.time.Months;
+import org.joda.time.MutableDateTime;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class BudgetActivity extends AppCompatActivity {
 
     private FloatingActionButton fb;
+    private DatabaseReference budgetRef;
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
 
+        mAuth = FirebaseAuth.getInstance();
+        // Adding items in the firebase database with json structure of budget then child is the uuid of the
+        // current user.
+        budgetRef = FirebaseDatabase.getInstance()
+                .getReference().child("budget").child(mAuth.getCurrentUser().getUid());
+        loader = new ProgressDialog(this);
         fb = findViewById(R.id.floatingActionButton);
         fb.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,17 +73,71 @@ public class BudgetActivity extends AppCompatActivity {
         Button Cancel_Btn = myView.findViewById(R.id.Cancel_button);
 
         Save_Btn.setOnClickListener(new View.OnClickListener() {
+
+            // Adding data in the form of an object to firebase Database.
+
             @Override
             public void onClick(View view) {
-                // Adding the items to the firebase database
 
+                // Adding the items to the firebase database
+                String Budget_Item;
+                if(amount_et.getText().toString().isEmpty()){
+                    amount_et.setError("Amount cannot be empty");
+                    return;
+                }
+
+                String Budget_Amount = amount_et.getText().toString();
+
+                if(sp.getSelectedItem().toString() == "Select item"){
+                    Toast.makeText(BudgetActivity.this,"Item needs to be selected",Toast.LENGTH_LONG).show();
+                    Budget_Item = "Other";
+                }else{
+                    loader.setMessage("Inserting an item in budget");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    Budget_Item = sp.getSelectedItem().toString();
+                    // Adding all the json values of an item in the firebase database.
+
+                    String id = budgetRef.push().getKey();
+                    // Adding the date
+                    DateFormat df = new SimpleDateFormat("dd-mm-yyyy");
+                    Calendar cal = Calendar.getInstance();
+                    String date = df.format(cal.getTime());
+
+                    MutableDateTime epoch =new MutableDateTime();
+                    // epoch to set at start of january 1990 and calculate accordingly
+                    epoch.setDate(0);
+                    DateTime now = new DateTime();
+                    Months month = Months.monthsBetween(epoch,now);
+                    Data_Added data_added = new Data_Added(id,Budget_Item,
+                            date,month.getMonths(), Integer.parseInt(Budget_Amount));
+                    // Add the data of type datacreated using budgetRef using firebasedatabase.
+                    Log.d("Ameya2", "onClick: Ameya"+Budget_Amount+Budget_Item);
+
+                    budgetRef.child(id).setValue(data_added).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(BudgetActivity.this,"Item added.",Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                Toast.makeText(BudgetActivity.this,"Item Failed to add to database.",Toast.LENGTH_LONG).show();
+                            }
+                            loader.dismiss();
+                        }
+                    });
+                }
+
+                dialog.dismiss();
             }
         });
 
         Cancel_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // Cancelling the dialog box.
+                dialog.dismiss();
             }
         });
         dialog.show();
